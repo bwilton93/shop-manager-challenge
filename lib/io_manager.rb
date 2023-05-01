a23b7c71a89837d22afab75781634261a4a94948
Barney <B.Wilton1993@gmail.com>
2023-05-01 12:40:13 +0100

    Wrote test for displaying items in a list

--- lib/io_manager.rb
+++ lib/io_manager.rb
@@ -6,23 +6,62 @@ class IOManager
   def initialize(io)
     @io = io
     @function_repo = FunctionRepo.new
+    @item_repo = ItemRepo.new
   end
 
   def run
     greeter
     list_options
+    case_handler(input)
+  end
+
+  private 
+
+  def input
+    return @io.gets.chomp
+  end
+
+  def case_handler(input)
+    new_line
+    case input
+    when "1"
+      list_items
+    when "2"
+
+    when "3"
+
+    when "4"
+    
+    else
+      exit
+    end
   end
   
   def greeter
     @io.puts "Welcome to the shop management program."
-    @io.puts ""
+    new_line
   end
-  
+
   def list_options
     @io.puts "What do you want to do?"
     functions = @function_repo.all
     functions.each do |function|
       @io.puts "#{function.id} = #{function.function}"
     end
+    new_line
+  end
+
+  def list_items
+    @io.puts "Here's a list of all shop items:"
+    new_line
+    items = @item_repo.all
+    items.each do |item|
+      @io.puts "#{item.id}. #{item.name} - Unit price: #{item.unit_price} - Quantity: #{item.quantity}"
+    end
+    new_line
+  end
+
+  def new_line
+    @io.puts ""
   end
 end
--- spec/io_manager_spec.rb
+++ spec/io_manager_spec.rb
@@ -1,7 +1,7 @@
 require 'io_manager'
 
 RSpec.describe IOManager do
-  it "returns the correct message flow" do
+  it "displays all items" do
     io = double :io
     expect(io).to receive(:puts).with("Welcome to the shop management program.")
     expect(io).to receive(:puts).with("")
@@ -10,7 +10,17 @@ RSpec.describe IOManager do
     expect(io).to receive(:puts).with("2 = Create a new item.")
     expect(io).to receive(:puts).with("3 = List all orders.")
     expect(io).to receive(:puts).with("4 = Create a new order.")
-
+    expect(io).to receive(:puts).with("")
+    expect(io).to receive(:gets).and_return("1")
+    expect(io).to receive(:puts).with("")
+    expect(io).to receive(:puts).with("Here's a list of all shop items:")
+    expect(io).to receive(:puts).with("")
+    expect(io).to receive(:puts).with("1. Star Wars Jedi: Survivor - Unit price: 60 - Quantity: 420")
+    expect(io).to receive(:puts).with("2. The Legend of Zelda: The Wind Waker - Unit price: 69 - Quantity: 14")
+    expect(io).to receive(:puts).with("3. Dead Space - Unit price: 42 - Quantity: 69")
+    expect(io).to receive(:puts).with("4. Metroid Prime - Unit price: 3 - Quantity: 12")
+    expect(io).to receive(:puts).with("")
+    
     io_manager = IOManager.new(io)
     io_manager.run
   end
